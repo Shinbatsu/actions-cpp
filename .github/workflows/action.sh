918792969ec66b7d552af24f3b000e918241b726
Shinbatsu <nojic132@gmail.com>
2023-01-01 17:58:07 +0300

    create workFlow

---
+++ .github/workflows/action.sh
@@ -0,0 +1,9 @@
+# #!/bin/sh
+
+# set -eux
+
+# sudo apt-get install clang-format &&
+# clang-format -style=llvm -dump-config > .clang-format &&
+# clang-format -i *.cpp &&
+
+echo "Hello from Actions"
